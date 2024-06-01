630092a53be0228106e3cdd4a8a8f0f5ece1cf38
mz-dz <tgrtgr389@gmail.com>
2024-05-31 21:05:06 -0700

    upload

---
+++ ibra/src/App.tsx
@@ -0,0 +1,56 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="sh">
+      👋 Hi, I’m Ibra <b>'front end'</b> and <b>'ui ux'</b>
+    </h1>
+    <h2 className="logo">
+      (☞ﾟヮﾟ)☞ All My Projects in My own{" "}
+      <a rel="prefetch" href="https://github.com/mz-dz">Github</a>{" "}
+    </h2>
+    <h3 id="-interests">👀 Interests</h3>
+    <ol>
+      <li>Crafting intuitive user interfaces</li>
+      <li>Designing delightful user experiences</li>
+      <li>Exploring the latest trends in front-end development</li>
+    </ol>
+    <h3 id="-currently-learning">🌱 Currently Learning</h3>
+    <ol className="c">
+      <li>Advanced CSS techniques</li>
+      <li>JavaScript frameworks like React js ⚛</li>
+      <li>Usability principles and user psychology</li>
+    </ol>
+    <h3 id="-looking-to-collaborate-on">💞️ Looking to Collaborate On</h3>
+    <ul>
+      <li>Front-end development projects</li>
+      <li>UI/UX design challenges</li>
+      <li>Open source initiatives focusing on user-centric design ology</li>
+    </ul>
+
+    <br />
+    <br />
+     😄 Pronouns
+- [Happy fan smart lovley and many more, e.g., He/Him, She/Her, They/Them]
+
+<br />
+<br />
+⚡ Fun Fact
+- [good]
+<br />
+<br />
+<button className="logo sh">I used **chatgpt 👩‍💻** to fast development with html css js react vite </button>
+<br />
+<br />
+<br />
+<a className="sh" href="mailto:tgrtgr389@gmail.com">📫 How to Reach Me</a>
+
+<p className="read-the-docs card">tgrtgr389@gmail.com</p>
+  </>
+  )
+}
+
+export default App
